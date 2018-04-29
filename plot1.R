65a9e7ab846cb4254445ad4a1d4b75ff8dc012c5
Mohd Tayyab <tayyabrahmani@gmail.com>
2018-04-29 19:21:32 +0530

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,19 @@
+#Loading Data
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+#Seeing Structure
+str(data)
+
+# convert the date variable to Date class
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+# Subset the data
+data <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+# Convert dates and times
+data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
+
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,26 @@
+#Loading Data
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+#Seeing Structure
+str(data)
+
+# convert the date variable to Date class
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+# Subset the data
+data <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+# Convert dates and times
+data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
+
+# Plot 3
+data$datetime <- as.POSIXct(data$datetime)
+attach(data)
+plot(Sub_metering_1 ~ datetime, type = "l",
+     ylab = "Energy Sub metering", xlab = "")
+lines(Sub_metering_2 ~ datetime, type = "l", col = "red")
+lines(Sub_metering_3 ~ datetime, type = "l", col = "blue")
+legend("topright", lty=1, lwd=2.5,col = c("black", "red", "blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.copy(png, file = "plot3.png", height = 480, width = 480)
+dev.off()
+detach(data)
